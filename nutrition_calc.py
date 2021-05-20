faa7156888782aa5cad808afe49e0c2b9a1bfa61
Vladimir Gorbovskiy <80061175+jabooticaba@users.noreply.github.com>
2021-05-20 22:33:35 +0700

    code wrapped in function

--- nutrition_calc.py
+++ nutrition_calc.py
@@ -1,27 +1,28 @@
 # Расчёт состава парэнтерального питания исходя из целевого объёма
 # Количество энергии вычисляется по формуле: 150 ккал на 1 г аминоазота (1 г АА = 1 г белка / 6,25)
 # Соотношение нутриентов вычисляется по формуле: 70% - глюкоза, 30% - жировые эмульсии
-
+ 
 from math import ceil
 
-weight = int(input('Вес пациента: '))
-volume = int(input('Целевой объём: '))
-volume_tmp = 0
-amin = 0
+def vol_calc(weight: float, volume: float, metab: int, concentr: int):
 
-metab = int(input('Метаболическое соотношение: '))
-concentr = int(input('Концентрация глюкозы: '))
+  '''weight - вес пациента в кг, volume - целевой объём ПЭП в мл, metab - метаболическое соотношение, concentr - концентрация глюкозы'''
 
-while volume_tmp < volume-1:  # Цикл подбора объёма
+  volume_tmp = 0
+  amin = 0
+
+  while volume_tmp < volume-1:  # Цикл подбора объёма
     amin = amin + 0.1  # Шаг цикла по подбору объёма
     kkal = (amin / 6.25 / 10) * metab  # Количество килокалорий
     glucose = ((kkal * 0.7) / 4) / (concentr / 100)  # Объём глюкозы нужной концентрации
     fat = ((kkal * 0.3) / 9) / 0.2  # Объём жиров концентрации 20%
     volume_tmp = amin + glucose + fat
-else:
+  else:
     print('================================')
-    print(f'Итоговый объём: ', math.ceil(volume_tmp))
+    print(f'Итоговый объём: ', ceil(volume_tmp))
     print(f'Глюкоза {concentr}%: {round(glucose)} мл')
     print(f'Липофундин 20%: {round(fat)} мл')
     print(f'Аминовен 10%: {round(amin)} мл')
     print(f'Белок: {round((amin / 10 / weight),1)} г/кг/сут, {round(kkal / weight)} ккал/кг/сут')
+
+vol_calc(5, 100, 150, 20)
